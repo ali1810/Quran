bb8ae2f1f19039671cbabf6238d7bac2be7add42
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2023-02-21 09:11:14 +0100

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-#from streamlit_option_menu import option_menu
-#import streamlit.components.v1 as html
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as html
 from  PIL import Image
 import numpy as np
 import pandas as pd
-#import streamlit.components.v1 as components
+import streamlit.components.v1 as components
 import base64
-#from streamlit_text_rating.st_text_rater import st_text_rater
+from streamlit_text_rating.st_text_rater import st_text_rater
 
 with st.sidebar:
     choose = option_menu("Main Menu", ["About", "Projects", "Blog","Apps", "Contact"],
