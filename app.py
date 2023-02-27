ee658bd5872b6420bc39d19be026dadcc98feb83
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2023-02-27 14:54:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def show_pdf(file_path):
      pdf_path = Path(file_path)
  #   with open(file_path,"rb") as f:
      base64_pdf = base64.b64encode(pdf_path.read_bytes()).decode('utf-8')
-     pdf_display = F"""<iframe src="data:application/pdf;base64,{base64_pdf}" width="800px" height="2100px" type="application/pdf"></iframe>
+     pdf_display = F"""<iframe src="application/pdf;base64,{base64_pdf}" width="800px" height="2100px" type="application/pdf"></iframe>
          """
      st.markdown(pdf_display, unsafe_allow_html=True)
 
